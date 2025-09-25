f175dbcb3a0bcb6b6895b2e26ba14794b267db49
jeanmnorhen <jeanmdasleite@gmail.com>
2025-09-25 01:01:18 -0300

    1

--- frontend-tester/babel.config.js
+++
@@ -1,8 +0,0 @@
-// frontend-tester/babel.config.js
-module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    ['@babel/preset-react', { runtime: 'automatic' }],
-    '@babel/preset-typescript',
-  ],
-};
